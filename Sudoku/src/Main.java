6b3665468c39ae7fa30c14e6db1fbdf8d9ce290e
kennycopas <kcopas2@hawkmail.hccfl.edu>
2024-09-20 21:20:58 -0400

    Add files via upload

--- Sudoku/src/Main.java
+++ Sudoku/src/Main.java
@@ -1,25 +1,34 @@
 import java.util.*;
 import java.lang.Math.*;
+
 public class Main {
+
     public static Square[][] map = new Square[9][9];
     public static Square[][] columns = new Square[9][9];
     public static Square[][][] groups = new Square[3][3][9];
     public static boolean boardComplete = false;
     public static int resetCount = 0;
+
     public static void populateMap() {
+
         //Populate map (acts as rows as well)
+
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 map[i][j] = new Square(i, j);
             }
         }
+
         //Populate columns
+
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 columns[i][j] = map[j][i];
             }
         }
+
         //Populate groups
+
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 3; j++) {
                 for(int k = 0; k < 3; k++) {
@@ -31,7 +40,9 @@ public class Main {
         }
     }
     public static void checkSingles() {
+
         //Populates any Square object that has only one possibles number value left
+
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 map[i][j].checkSingle();
@@ -39,8 +50,10 @@ public class Main {
         }
     }
     public static void populateSquares() {
+
         //While the board is incomplete, check for singles, assign the prioritized Square object, populate that object,
         //remove the number value assigned to that object from all the related objects, and check if the board is complete
+
         while(!boardComplete) {
             checkSingles();
             Square priority = priority();
@@ -51,7 +64,9 @@ public class Main {
         System.out.println();
     }
     public static void reset() {
+
         //Reset the entire board, populate the map again
+
         resetCount++;
         map = new Square[9][9];
         columns = new Square[9][9];
@@ -60,7 +75,9 @@ public class Main {
         populateMap();
     }
     public static void isBoardComplete() {
+
         //Check if ever Square object has an assigned value
+
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 if(map[i][j].num == 0) {
@@ -71,7 +88,9 @@ public class Main {
         boardComplete = true;
     }
     public static Square priority() {
+
         //Return the Square object with the least amount of possible number values left (possibles.size() returns the smallest number)
+
         int tinySize = 9;
         Square tinySquare = map[0][0];
         for(int i = 0; i < 9; i++) {
@@ -84,41 +103,56 @@ public class Main {
         return tinySquare;
     }
     public static void printMap() {
+
         System.out.println("  --------------------------------------------------------");
+
         for(int i = 0; i < 9; i++) {
+
             System.out.print("  |  ");
+
             for(int j = 0; j < 9; j++) {
                 System.out.print(map[i][j].num + "  |  ");
             }
+
             System.out.println();
             System.out.println("  --------------------------------------------------------");
+
         }
     }
     public static Square[][] getRelated(int x, int y) {
+
         //Returns a two-dimensional array that holds three arrays
         //Each array is a set of objects related to the object at the coordinates passed (x, y)
         //The three arrays are the row, column, and group of the referenced object
-        Square[][] related = new Square[3][9];
-        related[0] = map[y];
-        related[1] = columns[x];
         int groupX = (int)(Math.ceil((y+1.0)/3)-1);
         int groupY = (int)(Math.ceil((x+1.0)/3)-1);
+        Square[][] related = new Square[3][9];
+
+        related[0] = map[y];
+        related[1] = columns[x];
         related[2] = groups[groupX][groupY];
+
         return related;
     }
     public static void removeList(Square[] list, int removed) {
+
         //Remove the passed number from all the objects in the passed array
+
         for(int i = 0; i < 9; i++) {
             list[i].remove(removed);
         }
     }
     public static void removePossibles(Square square) {
+
         //Remove the assigned number value from all the related objects' lists of possible number (for each object: possibles.remove(Integer.valueOf(num)))
+
         int num = square.num;
         Square[][] related = getRelated(square.x, square.y);
+
         Square[] row = related[0];
         Square[] column = related[1];
         Square[] group = related[2];
+
         removeList(row, num);
         removeList(column, num);
         removeList(group, num);
--- Sudoku/src/Square.java
+++ Sudoku/src/Square.java
@@ -1,37 +1,52 @@
 import java.util.*;
+
 public class Square {
+
     //This ArrayList holds all the possible numbers that an instance of this class can be assigned in accordance with Sudoku rules
+
     public ArrayList<Integer> possibles = new ArrayList<>(10);
     public int x;
     public int y;
     public int num = 0;
     public Square(int y, int x) {
+
+        this.x = x;
+        this.y = y;
+
         //Populate the possibles ArrayList with {1,2,3,4,5,6,7,8,9}
+
         for(int i = 1; i < 10; i++) {
             possibles.add(i);
         }
-        this.x = x;
-        this.y = y;
     }
     public void checkSingle() {
+
         //If there is only one possible number left to be assigned, assign that number to this object
+
         if (possibles.size() == 1 && num == 0) {
             this.populate();
             Main.removePossibles(this);
         }
     }
     public void populate() {
+
         //Assign a random number from the possibles ArrayList to this object, if there are any numbers left in the possibles ArrayList
+
         int rand = (int)(Math.floor(Math.random() * possibles.size()));
+
         if(!possibles.isEmpty()) {
             this.num = possibles.get(rand);
         } else {
+
             //If the possibles ArrayList is empty, reset the board
+
             Main.reset();
         }
     }
     public void remove(int removed) {
+
         //Remove the passed number from the possibles ArrayList
+
         possibles.remove(Integer.valueOf(removed));
     }
 }
